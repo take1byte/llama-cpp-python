7503ae3b365cd1d3c9fdda346f80bc8a16874a4c
take1byte <electro.magnetism550@passfwd.com>
2024-10-28 23:06:52 -0400

    added llm_guard_predict for local chat with llm_guard protection

--- examples/gradio_chat/local.py
+++ examples/gradio_chat/local.py
@@ -8,6 +8,7 @@ from guard import logging_utils
 from guard.constants import INPUT_FORMAT_MSG, OUT_OF_SCOPE_MSG
 from guard.prompt_handler import split_instructions_and_data, rewrite
 from guard.response_handler import response_generator
+from llm_guard.input_scanners import BanTopics
 
 llama = llama_cpp.Llama.from_pretrained(
     repo_id="lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF",
@@ -21,7 +22,8 @@ model = "gpt-3.5-turbo"
 convo_id = str(uuid.uuid4())[:8]
 logger = logging_utils.build_logger("convo_log", f"convo_log_{convo_id}.log")
 
-SECURED = True
+SECURED = False
+LLM_GUARD = True
 
 
 def secured_predict(message, history, principal="guest@domain.com"):
@@ -62,6 +64,37 @@ def secured_predict(message, history, principal="guest@domain.com"):
             logger.info(f"is_permitted: False")
             for text in response_generator(OUT_OF_SCOPE_MSG):
                 yield text
+
+
+def llm_guard_predict(message, history):
+    messages = []
+
+    logger.info(f"user message:{message}")
+
+    for user_message, assistant_message in history:
+        messages.append({"role": "user", "content": user_message})
+        messages.append({"role": "assistant", "content": assistant_message})
+
+    scanner = BanTopics(topics=["violence"], threshold=0.5)
+    sanitized_prompt, is_valid, risk_score = scanner.scan(message)
+    logger.info(
+        f"sanitized_prompt: {sanitized_prompt}; is_valid: {is_valid}; risk_score: {risk_score}"
+    )
+
+    if is_valid:
+        messages.append({"role": "user", "content": sanitized_prompt})
+        response = llama.create_chat_completion_openai_v1(
+            model=model, messages=messages, stream=True
+        )
+        text = ""
+        for chunk in response:
+            content = chunk.choices[0].delta.content
+            if content:
+                text += content
+                yield text
+    else:
+        for text in response_generator(OUT_OF_SCOPE_MSG):
+            yield text
 
 
 def predict(message, history):
@@ -115,7 +148,14 @@ with gr.Blocks(theme=gr.themes.Soft(), js=js, css=css, fill_height=True) as demo
             "write an article about cybersecurity",
         ]
     )
-    predict_fn = secured_predict if SECURED else predict
+
+    if SECURED:
+        predict_fn = secured_predict
+    elif LLM_GUARD:
+        predict_fn = llm_guard_predict
+    else:
+        predict_fn = predict
+
     gr.ChatInterface(
         predict_fn,
         fill_height=True,
